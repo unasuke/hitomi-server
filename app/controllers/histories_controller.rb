c2ea112b30de71a5fa928f30edd9ad9dee7e14b8
Yusuke Nakamura <yusuke1994525@gmail.com>
2017-06-22 23:47:19 +0900

    Create actions index and create

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,2 +1,15 @@
 class HistoriesController < ApplicationController
+  def index
+    History.all
+  end
+
+  def create
+    History.create!(history_params)
+  end
+
+  private
+
+  def history_params
+    params.require(:history).permit(:title, :url, :raw_user_agent)
+  end
 end
