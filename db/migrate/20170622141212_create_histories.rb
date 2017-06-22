1f5793c1666f120376df030ce8564086cb55ec6c
Yusuke Nakamura <yusuke1994525@gmail.com>
2017-06-22 23:21:21 +0900

    Create histories table

--- db/migrate/20170622141212_create_histories.rb
+++ db/migrate/20170622141212_create_histories.rb
@@ -1,7 +1,9 @@
 class CreateHistories < ActiveRecord::Migration[5.1]
   def change
     create_table :histories do |t|
-
+      t.string :title, null: false
+      t.string :url, null: false
+      t.string :raw_user_agent, null: false
       t.timestamps
     end
   end
